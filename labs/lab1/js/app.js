f0cb297c60bcf38930d8e7756e88dd25056f4785
pmarchand122 <pmarchan@iu.edu>
2021-01-18 20:33:34 -0500

    added javascript

---
+++ labs/lab1/js/app.js
@@ -0,0 +1,17 @@
+let myName = "A. Name";
+let age = 42;
+console.log(myName);
+
+myName = "Pierre Marchand";
+console.log(myName);
+
+var a = 4;
+var b = 3;
+
+var c = a % b;
+console.log(c);
+
+let firstName = "Pierre";
+let lastName = "Marchand";
+let fullName = firstName + " " + lastName;
+console.log(fullName);
