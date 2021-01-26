371f6a632208cc13b8cb7b482191dee712b6972a
pmarchand122 <pmarchan@iu.edu>
2021-01-26 15:56:57 -0500

    Lab1

--- labs/lab1/js/app.js
+++ labs/lab1/js/app.js
@@ -1,17 +1,71 @@
-let myName = "A. Name";
-let age = 42;
-console.log(myName);
+function setup() {
+    var backgroundC = "#4ebde6";
+    var startX = 30;
 
-myName = "Pierre Marchand";
-console.log(myName);
+    createCanvas(800,600);
 
-var a = 4;
-var b = 3;
+    background(backgroundC);
 
-var c = a % b;
-console.log(c);
+    fill(0, 200, 50)
+    rect(70, 60, 300, 50);
 
-let firstName = "Pierre";
-let lastName = "Marchand";
-let fullName = firstName + " " + lastName;
-console.log(fullName);
+
+    fill(214, 41, 67);
+    circle(100, 100, 30);
+
+    stroke(2);
+    line(startX,40,200,20);
+    
+}
+
+/*let xPos = 0;
+let yPos = 40;
+
+let xSpeed = 1;
+let ySpeed = 1;
+
+function setup(){
+    createCanvas(400, 300);
+}*/
+
+
+/*function draw() {
+    background(250, 0 , 0);
+    xPos = xPos + xSpeed;
+    yPos = yPos + ySpeed;
+
+
+    if(xPos > 100) {
+        fill(0, 250, 0)
+    } else {
+        fill(0, 0, 0)
+    }
+
+
+    circle(xPos, yPos, 30);
+}*/
+
+/*function draw() {
+    background(62, 237, 108);
+    xPos = xPos + xSpeed;
+    yPos = yPos + ySpeed;
+
+    if(keyIsDown(LEFT_ARROW)) {
+        xSpeed = -1;
+    }
+
+    if(keyIsDown(RIGHT_ARROW)) {
+        xSpeed = 1;
+    }
+
+    if(keyIsDown(UP_ARROW)) {
+        ySpeed = -1;
+    }
+
+    if(keyIsDown(DOWN_ARROW)) {
+        ySpeed = 1;
+    }
+
+
+    circle(xPos, yPos, 30);
+}*/
